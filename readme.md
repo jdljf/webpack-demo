0c9ba786b4101fed66752c8460f5e358a31f0869
jdljf <1243270526@qq.com>
2019-04-20 15:54:45 +0800

    添加 NPM Scripts

---
+++ readme.md
@@ -0,0 +1,6 @@
+### 随意运行以下命令行命令
+```
+  npm run start
+  // 或者
+  npm run build
+```
--- webpack.common.js
+++ webpack.common.js
@@ -3,17 +3,17 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	plugin: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			title: 'Production'
-		})
-	],
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
